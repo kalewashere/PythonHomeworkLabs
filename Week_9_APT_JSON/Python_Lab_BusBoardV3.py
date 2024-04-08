7f08fab0d615216c818e3a6f86f8da7dbf2e3fda
kalewashere <mclaughlin.k.taylor@gmail.com>
2024-04-08 11:12:36 -0500

    update 3/24

---
+++ Week_9_APT_JSON/Python_Lab_BusBoardV3.py
@@ -0,0 +1,41 @@
+"""Starting from a clean slate - comments got overwhelming in first attempt. Going to try some functions here to see
+if those will help in this process - ok so couldnt get a function to work - need to review"""
+import requests
+
+url1 = 'https://svc.metrotransit.org/NexTrip/17940?format=json'
+url2 = 'http://svc.metrotransit.org/NexTrip/17928?format=json'
+
+nb_response = requests.get(url1).json()
+sb_response = requests.get(url2).json()
+
+nb_departures = nb_response['departures']
+sb_departures = sb_response['departures']
+
+print('Northbound buses buses arriving outside of Minneapolis College are scheduled as follows: ')
+
+print(f'Route      Time        Bus Description')
+
+for nb_bus_info in nb_departures:
+    nb_route = nb_bus_info['route_id']
+    nb_time = nb_bus_info['departure_text']
+    nb_direction = nb_bus_info['direction_text']
+    nb_description = nb_bus_info['description']
+    print(f'{nb_route:<10} {nb_time:<11} {nb_description}')
+print('')
+print('Southbound buses arriving outside of Minneapolis College are scheduled as follows: ')
+
+print(f'Route      Time        Bus Description')
+for sb_bus_info in sb_departures:
+    sb_route = sb_bus_info['route_id']
+    sb_time = sb_bus_info['departure_text']
+    sb_direction = sb_bus_info['direction_text']
+    sb_description = sb_bus_info['description']
+    print(f'{sb_route:<10} {sb_time:<11} {sb_description}')
+# todo guessing a while loop would be best here - brb - nope def not
+# def northbound(direction):
+#   if direction in nb_departures and sb_departures == 'NB':
+#        print('Northbound')
+#    else:
+#        print('South')
+
+# def main():
--- Week_9_APT_JSON/python_lab_busboardv2.py
+++ Week_9_APT_JSON/python_lab_busboardv2.py
@@ -1,1 +1,25 @@
-"""this was a test program """
+"""this was a test program - used to dump ideas and practice code to not overwhelm myself in lab"""
+
+import requests
+
+url1 = 'https://svc.metrotransit.org/NexTrip/17940?format=json'
+url2 = 'http://svc.metrotransit.org/NexTrip/17928?format=json'
+
+nb_response = requests.get(url1).json()
+sb_response = requests.get(url2).json()
+
+nb_departures = nb_response['departures']
+sb_departures = sb_response['departures']
+
+for bus_info in nb_departures:
+    route = bus_info['route_id']
+    time = bus_info['departure_text']
+    direction = bus_info['direction_text']
+    description = bus_info['description']
+    print(f'{route:<10} {time:<11} {description}')
+
+#    print('Northbound buses')
+#   print(f'Route      Time        Bus Description')
+
+#   print('Southbound buses')
+#   print(f'Route      Time        Bus Description')
